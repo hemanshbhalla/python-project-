cf0ba2e2c100f94f2280bacc24d5ffd34f31eb29
hemanshbhalla <82414799+hemanshbhalla@users.noreply.github.com>
2021-04-13 02:42:46 +0530

    Add files via upload

---
+++ task 1.py
@@ -0,0 +1,9 @@
+Python 3.9.0 (v3.9.0:9cf6752276, Oct  5 2020, 11:29:23) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> radius = float(input("Enter the radius: "))
+Enter the radius: 1.1
+>>> area = 3.14*radius*radius
+>>> print("The area of circle: " , area)
+The area of circle:  3.7994000000000008
+>>> 
